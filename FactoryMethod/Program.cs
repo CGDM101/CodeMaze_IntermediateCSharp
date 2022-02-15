7f18921bd8d6b705ab5b80ef3324d3a6aebbddaa
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-02-15 19:22:53 +0100

    Factory method final.

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,65 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FactoryMethod
 { // Creational.
 
-    public interface IAirConditioner
+    public enum Actions
     {
-        void Operate();
+        Cooling,
+        Warming
     }
 
-    public class CoolingManager : IAirConditioner
+    public class AirConditioner
     {
-        private readonly double _temperature;
+        private readonly Dictionary<Actions, AirConditionerFactory> _factories;
 
-        public CoolingManager(double temperature)
+        private AirConditioner()
         {
-            _temperature = temperature;
+            //_factories = new Dictionary<Actions, AirConditionerFactory>
+            //{
+            //    { Actions.Cooling, new CoolingFactory() },
+            //    { Actions.Warming, new WarmingFactory() }
+            //};
+            _factories = new Dictionary<Actions, AirConditionerFactory>();
+
+            foreach (Actions action in Enum.GetValues(typeof(Actions)))
+            {
+                var factory = (AirConditionerFactory)Activator.CreateInstance(Type.GetType("FactoryMethod." + Enum.GetName(typeof(Actions), action) + "Factory"));
+                _factories.Add(action, factory);
+            }
         }
 
-        public void Operate()
-        {
-            Console.WriteLine($"Cooling the room to the required temperature of {_temperature} degrees");
-        }
+        // To be able to chain methods in main:
+        public static AirConditioner InitialiseFactories() => new AirConditioner();
+
+        public IAirConditioner ExecuteCreatiion(Actions action, double temperature) => _factories[action].Create(temperature);
     }
-
-    public class WarmingManager : IAirConditioner
-    {
-        private readonly double _temperature;
-
-        public WarmingManager(double temperature)
+        public interface IAirConditioner
         {
-            _temperature = temperature;
+            void Operate();
         }
 
-        public void Operate()
+        public class CoolingManager : IAirConditioner
         {
-            Console.WriteLine($"Warming the room to the required temperature of {_temperature} degrees");
+            private readonly double _temperature;
+
+            public CoolingManager(double temperature)
+            {
+                _temperature = temperature;
+            }
+
+            public void Operate()
+            {
+                Console.WriteLine($"Cooling the room to the required temperature of {_temperature} degrees");
+            }
+        }
+
+        public class WarmingManager : IAirConditioner
+        {
+            private readonly double _temperature;
+
+            public WarmingManager(double temperature)
+            {
+                _temperature = temperature;
+            }
+
+            public void Operate()
+            {
+                Console.WriteLine($"Warming the room to the required temperature of {_temperature} degrees");
+            }
+        }
+
+        // A factory creator for the cooling / warming managager classes:
+        // Provides an interface for object creation in derived classes.
+        public abstract class AirConditionerFactory
+        {
+            public abstract IAirConditioner Create(double temperature);
+        }
+
+        public class CoolingFactory : AirConditionerFactory
+        {
+            public override IAirConditioner Create(double temperature) => new CoolingManager(temperature); // Våra två factory-metoder Create
+        }
+
+        public class WarmingFactory : AirConditionerFactory
+        {
+            public override IAirConditioner Create(double temperature) => new WarmingManager(temperature);
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            //var factory = new AirConditioner().ExecuteCreatiion(Actions.Cooling, 22.5);
+            //factory.Operate();
+            // chaining:
+            AirConditioner
+            .InitialiseFactories()
+            .ExecuteCreatiion(Actions.Cooling, 22.5)
+            .Operate();
+            }
         }
     }
-
-    // A factory creator for the cooling / warming managager classes:
-    // Provides an interface for object creation in derived classes.
-    public abstract class AirConditionerFactory
-    {
-        public abstract IAirConditioner Create(double temperature);
-    }
-
-    public class CoolingFactory : AirConditionerFactory
-    {
-        public override IAirConditioner Create(double temperature) => new CoolingManager(temperature); // Våra två factory-metoder Create
-    }
-
-    public class WarmingFactory : AirConditionerFactory
-    {
-        public override IAirConditioner Create(double temperature) => new WarmingManager(temperature);
-    }
-
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
