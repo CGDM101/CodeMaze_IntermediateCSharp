59855a22e9dbfc4cf9afd880ef1215e906854f24
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-03-02 16:39:23 +0100

    Refactoring according to SR P.

--- SR_P/Program.cs
+++ SR_P/Program.cs
@@ -25,6 +25,8 @@ namespace SR_P
 
         public void RemoveEntryAt(int index) => _entries.RemoveAt(index); // 3. Remove entries.
 
+        // Removed into new class and modified.
+        /*
         public void SaveToFile(string directoryPath, string fileName) // 3. Save entries to file.
         {
             if (!Directory.Exists(directoryPath))
@@ -33,20 +35,41 @@ namespace SR_P
             }
 
             File.WriteAllText(Path.Combine(directoryPath, fileName), ToString());
-        }
+        }*/
 
         public override string ToString() =>
                 string.Join(Environment.NewLine, _entries.Select(x => $"Code: {x.ProjectCode}, Name: {x.ProjectName}, Hours: {x.SpentHours}"));
+    }
 
+    public class FileSaver // New class for filesaving
+    {
+        public void SaveToFile(string directoryPath, string fileName, WorkReport report) // obs workreport cs!
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(Path.Combine(directoryPath, fileName), report.ToString()); // workreport
+        }
     }
 
     class Program
     {
         // Maintainable and readable code - each and every class should do its own task! Not responsible for several different tasks (then it is more likely to change in the future)!
         // SRP - A class should have only one reason to change - Only one responsibility!
+        // Separation of concerns!
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var report = new WorkReport();
+            report.AddEntry(new WorkReportEntry { ProjectCode = "123Ds", ProjectName = "Project1", SpentHours = 5 });
+            report.AddEntry(new WorkReportEntry { ProjectCode = "987FC", ProjectName = "Project2", SpentHours = 3 });
+
+            Console.WriteLine(report.ToString());
+
+            var saver = new FileSaver();
+            saver.SaveToFile(@"Reports", "WorkReport.txt", report);
+
         }
     }
 }
