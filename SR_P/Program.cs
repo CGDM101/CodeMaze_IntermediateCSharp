da87ccf7113ef803b798423a276e15493d577c77
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-03-02 17:16:49 +0100

    SRP - refactoring further with an interface.

--- SR_P/Program.cs
+++ SR_P/Program.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using System.IO; // Directory, Path
 using System.Linq;
+using static SR_P.FileSaver;
 
 namespace SR_P
 {
+    public interface IEntryManager<T>  // add to workreport
+    {
+        void AddEntry(T entry);
+        void RemoveEntryAt(int index);
+    }
     public class WorkReportEntry
     {
         public string ProjectCode { get; set; }
@@ -12,7 +18,8 @@ namespace SR_P
         public int SpentHours { get; set; }
     }
 
-    public class WorkReport // Has many responsibilities related to workreporting. More can be added also... So it has more than one reason to change in the future, e g if we want to save a file in another way...
+    public class WorkReport : IEntryManager<WorkReportEntry> // Interface is added
+                                                             // Has many responsibilities related to workreporting. More can be added also... So it has more than one reason to change in the future, e g if we want to save a file in another way...
     {
         private readonly List<WorkReportEntry> _entries;
 
@@ -43,6 +50,7 @@ namespace SR_P
 
     public class FileSaver // New class for filesaving
     {
+        /*
         public void SaveToFile(string directoryPath, string fileName, WorkReport report) // obs workreport cs!
         {
             if (!Directory.Exists(directoryPath))
@@ -51,6 +59,42 @@ namespace SR_P
             }
 
             File.WriteAllText(Path.Combine(directoryPath, fileName), report.ToString()); // workreport
+        }*/
+
+        // After interface.
+        public void SaveToFile<T>(string directoryPath, string fileName, IEntryManager<T> workReport)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(Path.Combine(directoryPath, fileName), workReport.ToString()); // workreport is T now
+        }
+
+        public class ScheduleTask // Simple to add this class/feature/task if we have the new interface.
+        {
+            public int TaskId { get; set; }
+            public string Content { get; set; }
+            public DateTime ExecuteOn { get; set; }
+        }
+
+        public class Scheduler : IEntryManager<ScheduleTask>
+        {
+            private readonly List<ScheduleTask> _scheduleTasks;
+
+            public Scheduler()
+            {
+                _scheduleTasks = new List<ScheduleTask>();
+            }
+
+            public void AddEntry(ScheduleTask entry) => _scheduleTasks.Add(entry);
+
+            public void RemoveEntryAt(int index) => _scheduleTasks.RemoveAt(index);
+
+            public override string ToString() =>
+                string.Join(Environment.NewLine, _scheduleTasks.Select(x => $"Task with id: {x.TaskId} with content: {x.Content} is going to be executed on: {x.ExecuteOn}"));
+
         }
     }
 
@@ -59,17 +103,30 @@ namespace SR_P
         // Maintainable and readable code - each and every class should do its own task! Not responsible for several different tasks (then it is more likely to change in the future)!
         // SRP - A class should have only one reason to change - Only one responsibility!
         // Separation of concerns!
+        // Testing becomes easier.
+        // Decoupled classes - independent of eachother.
+        // Methods become highly related (coherent) - different methods are joinged to do one thing and do it well.
+
+        // In projects that are already written, it is difficult to implement SRP.
+        // Complact classes with tiny methods, but that can create organisational risk! - Group them well!
         static void Main(string[] args)
         {
             var report = new WorkReport();
             report.AddEntry(new WorkReportEntry { ProjectCode = "123Ds", ProjectName = "Project1", SpentHours = 5 });
             report.AddEntry(new WorkReportEntry { ProjectCode = "987FC", ProjectName = "Project2", SpentHours = 3 });
 
+            //
+            var scheduler = new Scheduler();
+            scheduler.AddEntry(new ScheduleTask { TaskId = 1, Content = "Do something now.", ExecuteOn = DateTime.Now.AddDays(5) });
+            scheduler.AddEntry(new ScheduleTask { TaskId = 2, Content = "Don't forget to...", ExecuteOn = DateTime.Now.AddDays(2) });
+            //
+
             Console.WriteLine(report.ToString());
+            Console.WriteLine(scheduler.ToString()); //
 
             var saver = new FileSaver();
             saver.SaveToFile(@"Reports", "WorkReport.txt", report);
-
+            saver.SaveToFile(@"Schedulers", "Schedule.txt", scheduler); //
         }
     }
 }
