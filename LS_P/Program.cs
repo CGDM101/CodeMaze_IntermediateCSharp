5f9da4d1d030829909c031a10099ec625a1f1280
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-03-10 14:53:00 +0100

    LS P implemented.

--- LS_P/Program.cs
+++ LS_P/Program.cs
@@ -3,26 +3,38 @@ using System.Linq; // Sum()
 
 namespace LS_P
 {
-    public class SumCalculator
+    public class SumCalculator:Calculator
     {
-        protected readonly int[] _numbers;
+        protected readonly int[] numbers;
 
         public SumCalculator(int[] numbers)
+            :base(numbers)
         {
-            _numbers = numbers;
         }
 
-        public virtual int Calculate() => _numbers.Sum();
+        public override int Calculate() => _numbers.Sum();
     }
 
-    public class EvenNumbersSumCalculator : SumCalculator
+    public class EvenNumbersSumCalculator : Calculator
     {
         public EvenNumbersSumCalculator(int[] numbers)
             :base(numbers)
         {
         }
 
-        public override int Calculate() => _numbers.Where(x => x % 2 == 0).Sum(); // new keyword because hiding the inherited member Calculate() is intended.
+        public override int Calculate() => _numbers.Where(x => x % 2 == 0).Sum(); 
+    }
+
+    public abstract class Calculator
+    {
+        protected readonly int[] _numbers;
+
+        public Calculator(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        public abstract int Calculate(); // ingen kropp.
     }
 
     class Program
@@ -32,19 +44,21 @@ namespace LS_P
         {
             var numbers = new int[] { 5, 7, 9, 8, 1, 6, 4 };
 
-            SumCalculator sum = new SumCalculator(numbers);
-            Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}"); // 40
+            // PROBLEM:
+            //SumCalculator evenSum2 = new EvenNumbersSumCalculator(numbers);
+            //Console.WriteLine($"The sum of all the even numbers: {evenSum2.Calculate()}"); // 40 - fel!
+
+            // If a child class inherits from a parent class, the child IS A parent class. Calculate() from the higher order base is executed. This child is not behaving as a substitute for the parent class!
+
+            // After adding virtual in parent and overrides it in child, the child's Calculate() will be used instead (so the result of evenSum2 will correctly be 18).
+            // Still, the problem is that the derived class cannot replace the base class - LSP is not implemented. Solution - an abstract class Calculator.
+            Calculator sumLsp = new SumCalculator(numbers);
+            Console.WriteLine($"The sum of all the numbers: {sumLsp.Calculate()}");
 
             Console.WriteLine();
 
-            EvenNumbersSumCalculator evenSum = new EvenNumbersSumCalculator(numbers);
-            Console.WriteLine($"The sum of all the even numbers: {evenSum.Calculate()}"); // 18
-
-            // PROBLEM:
-            SumCalculator evenSum2 = new EvenNumbersSumCalculator(numbers);
-            Console.WriteLine($"The sum of all the even numbers: {evenSum2.Calculate()}"); // 40 - fel!
-
-            // If a child class inherits from a parent class, the child IS A parent class. Calculate() from the higher order base is executed. This child is not behaving as a substitute for the parent class!
+            Calculator evenSumLsp = new EvenNumbersSumCalculator(numbers);
+            Console.WriteLine($"The sum of all the even numbers: {evenSumLsp.Calculate()} ");
         }
     }
 }
